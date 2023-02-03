6b709c5df37b588c7e2268dab1ac8557a8fb9e2b
EngineerSk <eskayoriade@gmail.com>
2023-02-03 07:30:49 +0100

    AppComponent dependency extracted from ActivityModule and declared as Component annotation dependency array parameter of ActivityComponent

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/activity/ActivityModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/activity/ActivityModule.kt
@@ -2,15 +2,13 @@ package com.techyourchance.dagger2course.common.dependencyInjection.activity
 
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import com.techyourchance.dagger2course.common.dependencyInjection.app.AppComponent
 import com.techyourchance.dagger2course.screens.common.ScreensNavigator
 import dagger.Module
 import dagger.Provides
 
 @Module
 class ActivityModule(
-    private val activity: AppCompatActivity,
-    private val appComponent: AppComponent
+    private val activity: AppCompatActivity
 ) {
 
     @Provides
@@ -21,11 +19,8 @@ class ActivityModule(
     fun screensNavigator(activity: AppCompatActivity):ScreensNavigator = ScreensNavigator(activity)
 
     @Provides
-    fun fragmentManager() = activity.supportFragmentManager
+    fun fragmentManager(activity: AppCompatActivity) = activity.supportFragmentManager
 
     @Provides
-    fun layoutInflater(): LayoutInflater = LayoutInflater.from(activity)
-
-    @Provides
-    fun stackoverflowApi() = appComponent.stackoverflowApi()
+    fun layoutInflater(activity: AppCompatActivity): LayoutInflater = LayoutInflater.from(activity)
 }
--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/app/AppComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyInjection/app/AppComponent.kt
@@ -3,7 +3,6 @@ package com.techyourchance.dagger2course.common.dependencyInjection.app
 import android.app.Application
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import dagger.Component
-import retrofit2.Retrofit
 
 @AppScope
 @Component(modules = [AppModule::class])
